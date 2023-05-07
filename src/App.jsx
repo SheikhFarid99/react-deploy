0aba7452197c85cfead73e373bc3dceabc4cdc3f
SheikhFarid99 <sheikhfarid498@gmail.com>
2023-05-07 23:11:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+function App() {
+  const [state, setState] = useState([])
+  const [text, setText] = useState('')
+
+  const add = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('http://localhost:5000/add-name', { name: text }, { withCredentials: true })
+      setState([...state, data])
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  const getData = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:5000/', { withCredentials: true })
+      setState(data.data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  return (
+    <div className="App">
+      <form onSubmit={add}>
+        <input onChange={(e) => setText(e.target.value)} type="text" className='px-4 py-3 w-[400px] border' placeholder="input" />
+        <button className="px-4 py-3 bg-indigo-500">submit</button>
+      </form>
+      <div>
+        {
+          state.map((d, i) => <h2 key={i}>{d.name}</h2>)
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
